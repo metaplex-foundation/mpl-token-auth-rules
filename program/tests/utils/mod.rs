a31246292067d617b4f7ec3e3daf67912038aa4d
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2022-11-29 16:07:11 -0800

    Replacing HashMap with PayloadVec type
    * This works around a Solita SDK generation issue.
    * Also abstracts initializing and indexing into the Vec from caller.
    * Would use an array but causes issues with Borsh serialization.

--- program/tests/utils/mod.rs
+++ program/tests/utils/mod.rs
@@ -2,7 +2,7 @@ use solana_program_test::ProgramTest;
 
 pub fn program_test() -> ProgramTest {
     ProgramTest::new(
-        "token-authorization-rules",
+        "token_authorization_rules",
         token_authorization_rules::id(),
         None,
     )
