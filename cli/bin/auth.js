c7eda25f68765d94d858a988ccfb29875bd9f4bb
blockiosaurus <blockiosaurus@gmail.com>
2022-12-01 13:51:59 -0500

    Removing build code.

--- cli/bin/auth.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const msgpack_1 = require("@msgpack/msgpack");
-const encoded = (0, msgpack_1.encode)({ foo: 'bar' });
-console.log(encoded);
-const decoded = (0, msgpack_1.decode)(encoded);
-console.log(decoded);
