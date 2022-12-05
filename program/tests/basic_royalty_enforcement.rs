91399de7d7e5f6deb41fb39cb0d1f337d7cc4307
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2022-12-05 00:09:54 -0800

    Update basic royalty enforcement test to check Transfer operation

--- program/tests/basic_royalty_enforcement.rs
+++ program/tests/basic_royalty_enforcement.rs
@@ -5,8 +5,13 @@ pub mod utils;
 use rmp_serde::Serializer;
 use serde::Serialize;
 use solana_program_test::tokio;
-use solana_sdk::{signature::Signer, transaction::Transaction};
-use token_authorization_rules::state::{Operation, Rule, RuleSet};
+use solana_sdk::{
+    signature::Signer, signer::keypair::Keypair, system_instruction, transaction::Transaction,
+};
+use token_authorization_rules::{
+    state::{Operation, Rule, RuleSet},
+    Payload,
+};
 use utils::program_test;
 
 #[tokio::test]
@@ -20,7 +25,7 @@ async fn basic_royalty_enforcement() {
     );
 
     // Rule for Transfers: Allow transfers to a Token Owned Escrow account.
-    let program_is_token_metadata = Rule::ProgramOwned {
+    let owned_by_token_metadata = Rule::ProgramOwned {
         program: mpl_token_metadata::id(),
     };
 
@@ -34,7 +39,7 @@ async fn basic_royalty_enforcement() {
 
     // Create Basic Royalty Enforcement Ruleset.
     let mut basic_royalty_enforcement_rule_set = RuleSet::new();
-    basic_royalty_enforcement_rule_set.add(Operation::Transfer, program_is_token_metadata);
+    basic_royalty_enforcement_rule_set.add(Operation::Transfer, owned_by_token_metadata);
     basic_royalty_enforcement_rule_set.add(Operation::Delegate, leaf_in_marketplace_tree.clone());
     basic_royalty_enforcement_rule_set.add(Operation::SaleTransfer, leaf_in_marketplace_tree);
 
@@ -72,4 +77,59 @@ async fn basic_royalty_enforcement() {
         .process_transaction(create_tx)
         .await
         .expect("creation should succeed");
+
+    // Create an account owned by token-metadata to simulate a Token-Owned Escrow account.
+    let fake_token_metadata_owned_escrow = Keypair::new();
+    let rent = context.banks_client.get_rent().await.unwrap();
+    let tx = Transaction::new_signed_with_payer(
+        &[system_instruction::create_account(
+            &context.payer.pubkey(),
+            &fake_token_metadata_owned_escrow.pubkey(),
+            rent.minimum_balance(0),
+            0,
+            &mpl_token_metadata::id(),
+        )],
+        Some(&context.payer.pubkey()),
+        &[&context.payer, &fake_token_metadata_owned_escrow],
+        context.last_blockhash,
+    );
+
+    context.banks_client.process_transaction(tx).await.unwrap();
+
+    // Store the payload of data to validate against the rule definition.
+    // In this case the destination address will be used to look up the
+    // `AccountInfo` and see who the owner is.
+    let payload = Payload::new(
+        Some(fake_token_metadata_owned_escrow.pubkey()),
+        None,
+        None,
+        None,
+    );
+
+    // Create a `validate` instruction.
+    let validate_ix = token_authorization_rules::instruction::validate(
+        token_authorization_rules::id(),
+        context.payer.pubkey(),
+        ruleset_addr,
+        "basic_royalty_enforcement".to_string(),
+        Operation::Transfer,
+        payload,
+        vec![],
+        vec![fake_token_metadata_owned_escrow.pubkey()],
+    );
+
+    // Add it to a transaction.
+    let validate_tx = Transaction::new_signed_with_payer(
+        &[validate_ix],
+        Some(&context.payer.pubkey()),
+        &[&context.payer],
+        context.last_blockhash,
+    );
+
+    // Process the transaction.
+    context
+        .banks_client
+        .process_transaction(validate_tx)
+        .await
+        .expect("validation should succeed");
 }
