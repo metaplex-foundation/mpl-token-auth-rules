7493263eaff1ec93af9929c598ef903917ff5cd3
blockiosaurus <blockiosaurus@gmail.com>
2022-12-06 20:37:11 -0500

    Adding baseline validation.

--- cli/src/auth.ts
+++ cli/src/auth.ts
@@ -1,9 +1,11 @@
 import { encode, decode } from '@msgpack/msgpack';
-import { createTokenAuthorizationRules } from './helpers/token-authorization-rules';
+import { createTokenAuthorizationRules, validateOperation } from './helpers/token-authorization-rules';
 import { Keypair, Connection, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
 import { Command, program } from "commander";
 import log from "loglevel";
 import * as fs from 'fs';
+import { findRuleSetPDA } from './helpers/pda';
+import { Payload, Operation } from '../../packages/sdk/src/generated';
 
 program
     .command("create")
@@ -51,28 +53,34 @@ program
     .option("-k, --keypair <path>", `Solana wallet location`)
     .option("-l, --log-level <string>", "log level", setLogLevel)
     .option("-n, --name <string>", "The name of the ruleset.")
-    .option("-rs, --ruleset <string>", "The ruleset json file.")
+    .option("-op, --operation <string>", "The operation to validate.")
+    .option("-da, --destination_address <string>", "The destination address.")
+    .option("-ds, --derived_seeds <items>", "The derivation seeds as a comma-separated list.")
+    .option("-am, --amount <int>", "The amount.")
+    .option("-tl, --tree_leaf <string>", "The merkle tree leaf.")
+    .option("-tp, --tree_proof <items>", "The merkle tree proof as a comma-separated list.")
     .action(async (directory, cmd) => {
-        const { keypair, env, rpc, name, ruleset } = cmd.opts();
+        const { keypair, env, rpc, name,
+            operation, destination_address, derived_seeds, amount, tree_leaf, tree_proof } = cmd.opts();
         let payer = loadKeypair(keypair);
         const connection = new Connection(rpc, "finalized");
 
-        // Airdrop some Sol if we're on localnet.
-        if (rpc == "http://localhost:8899") {
-            const airdropSignature = await connection.requestAirdrop(
-                payer.publicKey,
-                10 * LAMPORTS_PER_SOL
-            );
-            await connection.confirmTransaction(airdropSignature);
-        }
+        console.log("Operation: " + operation);
+        console.log("Destination Address: " + destination_address);
+        console.log("Derived Seeds: " + derived_seeds);
+        console.log("Amount: " + amount);
+        console.log("Tree Leaf: " + tree_leaf);
+        console.log("Tree Proof: " + tree_proof);
 
-        const rulesetFile = JSON.parse(fs.readFileSync(ruleset, 'utf-8'));
-
-        const encoded = encode(rulesetFile);
-        let rulesetAddress = await createTokenAuthorizationRules(connection, payer, name, encoded);
-        let rulesetData = await connection.getAccountInfo(rulesetAddress);
-        let rulesetDecoded = decode(rulesetData?.data);
-        console.log("RuleSet Decoded: " + JSON.stringify(rulesetDecoded, null, 2));
+        let payload: Payload = {
+            amount,
+            destinationKey: new PublicKey(destination_address),
+            derivedKeySeeds: null,
+            treeMatchLeaf: null,
+        };
+        payload.amount = amount;
+        let result = await validateOperation(connection, payer, name, operation, payload);
+        console.log("Result: " + result);
     });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
--- cli/src/helpers/token-authorization-rules.ts
+++ cli/src/helpers/token-authorization-rules.ts
@@ -6,8 +6,9 @@ import {
     SYSVAR_INSTRUCTIONS_PUBKEY,
     Transaction,
 } from "@solana/web3.js";
-import { createCreateInstruction, PROGRAM_ID } from "../../../packages/sdk/src/token-authorization-rules";
+import { createCreateInstruction, createValidateInstruction, Operation, Payload, PROGRAM_ID } from "../../../packages/sdk/src/token-authorization-rules";
 import { findRuleSetPDA } from "./pda";
+import { TokenMetadataProgram } from "@metaplex-foundation/js";
 
 export const createTokenAuthorizationRules = async (
     connection: Connection,
@@ -37,4 +38,53 @@ export const createTokenAuthorizationRules = async (
     const sig = await connection.sendTransaction(tx, [payer], { skipPreflight: true });
     await connection.confirmTransaction(sig, "finalized");
     return ruleSetAddress[0];
+}
+
+export const validateOperation = async (
+    connection: Connection,
+    payer: Keypair,
+    name: string,
+    operation: string,
+    payload: Payload,
+) => {
+
+    let op_type: Operation = Operation.Transfer;
+    switch (operation) {
+        case "Transfer":
+            op_type = Operation.Transfer;
+            break;
+        case "Delegate":
+            op_type = Operation.Delegate;
+            break;
+        case "SaleTransfer":
+            op_type = Operation.SaleTransfer;
+            break;
+        case "MigrateClass":
+            op_type = Operation.MigrateClass;
+            break;
+    }
+    const ruleSetAddress = await findRuleSetPDA(payer.publicKey, name);
+    let validateIX = createValidateInstruction(
+        {
+            payer: payer.publicKey,
+            ruleset: ruleSetAddress[0],
+            systemProgram: SystemProgram.programId,
+        },
+        {
+            validateArgs: {
+                name,
+                operation: op_type,
+                payload
+            },
+        },
+        PROGRAM_ID,
+    );
+
+    const tx = new Transaction().add(validateIX);
+
+    const { blockhash } = await connection.getLatestBlockhash();
+    tx.recentBlockhash = blockhash;
+    tx.feePayer = payer.publicKey;
+    const sig = await connection.sendTransaction(tx, [payer], { skipPreflight: true });
+    await connection.confirmTransaction(sig, "finalized");
 }
