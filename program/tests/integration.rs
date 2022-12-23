026fd82e4ba53fdb534f49555cd4d8f8d4c94acc
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2022-12-23 12:11:28 -0800

    Move some common test functionality to utils

--- program/tests/integration.rs
+++ program/tests/integration.rs
@@ -12,8 +12,6 @@ use mpl_token_auth_rules::{
     state::{CompareOp, Rule, RuleSet},
 };
 use num_traits::cast::FromPrimitive;
-use rmp_serde::Serializer;
-use serde::Serialize;
 use solana_program::instruction::{AccountMeta, InstructionError};
 use solana_program_test::{tokio, BanksClientError};
 use solana_sdk::{
@@ -21,7 +19,10 @@ use solana_sdk::{
     signer::keypair::Keypair,
     transaction::{Transaction, TransactionError},
 };
-use utils::{program_test, Operation};
+use utils::{
+    create_rule_set_on_chain, process_failing_validate_ix, process_passing_validate_ix,
+    program_test, Operation,
+};
 
 #[tokio::test]
 async fn test_payer_not_signer_fails() {
@@ -96,19 +97,14 @@ async fn test_payer_not_signer_fails() {
 async fn test_additional_signer_and_amount() {
     let mut context = program_test().start_with_context().await;
 
-    // Find RuleSet PDA.
-    let (rule_set_addr, _rule_set_bump) = mpl_token_auth_rules::pda::find_rule_set_address(
-        context.payer.pubkey(),
-        "test rule_set".to_string(),
-    );
-
-    // Second signer.
-    let second_signer = Keypair::new();
-
     // Create some rules.
     let adtl_signer = Rule::AdditionalSigner {
         account: context.payer.pubkey(),
     };
+
+    // Second signer.
+    let second_signer = Keypair::new();
+
     let adtl_signer2 = Rule::AdditionalSigner {
         account: second_signer.pubkey(),
     };
@@ -136,36 +132,9 @@ async fn test_additional_signer_and_amount() {
 
     println!("{:#?}", rule_set);
 
-    // Serialize the RuleSet using RMP serde.
-    let mut serialized_rule_set = Vec::new();
-    rule_set
-        .serialize(&mut Serializer::new(&mut serialized_rule_set))
-        .unwrap();
-
-    // Create a `create` instruction.
-    let create_ix = CreateOrUpdateBuilder::new()
-        .payer(context.payer.pubkey())
-        .rule_set_pda(rule_set_addr)
-        .build(CreateOrUpdateArgs::V1 {
-            serialized_rule_set,
-        })
-        .unwrap()
-        .instruction();
-
-    // Add it to a transaction.
-    let create_tx = Transaction::new_signed_with_payer(
-        &[create_ix],
-        Some(&context.payer.pubkey()),
-        &[&context.payer],
-        context.last_blockhash,
-    );
-
-    // Process the transaction.
-    context
-        .banks_client
-        .process_transaction(create_tx)
-        .await
-        .expect("creation should succeed");
+    // Put the RuleSet on chain.
+    let rule_set_addr =
+        create_rule_set_on_chain(&mut context, rule_set, "test rule_set".to_string()).await;
 
     // Create a Keypair to simulate a token mint address.
     let mint = Keypair::new().pubkey();
@@ -189,20 +158,8 @@ async fn test_additional_signer_and_amount() {
         .unwrap()
         .instruction();
 
-    // Add it to a transaction.
-    let validate_tx = Transaction::new_signed_with_payer(
-        &[validate_ix],
-        Some(&context.payer.pubkey()),
-        &[&context.payer],
-        context.last_blockhash,
-    );
-
-    // Process the transaction.
-    let err = context
-        .banks_client
-        .process_transaction(validate_tx)
-        .await
-        .expect_err("validation should fail");
+    // Fail to validate Transfer operation.
+    let err = process_failing_validate_ix(&mut context, validate_ix, vec![]).await;
 
     // Deconstruct the error code and make sure it is what we expect.
     match err {
@@ -232,25 +189,13 @@ async fn test_additional_signer_and_amount() {
         .unwrap()
         .instruction();
 
-    // Add it to a transaction.
-    let validate_tx = Transaction::new_signed_with_payer(
-        &[validate_ix],
-        Some(&context.payer.pubkey()),
-        &[&context.payer, &second_signer],
-        context.last_blockhash,
-    );
-
-    // Process the transaction, this time it should succeed.
-    context
-        .banks_client
-        .process_transaction(validate_tx)
-        .await
-        .expect("validation should succeed");
+    // Validate Transfer operation.
+    process_passing_validate_ix(&mut context, validate_ix, vec![&second_signer]).await;
 
     // Store a payload of data with the WRONG amount.
     let payload = Payload::from([(PayloadKey::Amount, PayloadType::Number(1))]);
 
-    // Create a `validate` instruction WITH the second signer.
+    // Create a `validate` instruction WITH the second signer.  Will fail because of WRONG amount.
     let validate_ix = ValidateBuilder::new()
         .rule_set_pda(rule_set_addr)
         .mint(mint)
@@ -266,20 +211,8 @@ async fn test_additional_signer_and_amount() {
         .unwrap()
         .instruction();
 
-    // Add it to a transaction.
-    let validate_tx = Transaction::new_signed_with_payer(
-        &[validate_ix],
-        Some(&context.payer.pubkey()),
-        &[&context.payer, &second_signer],
-        context.last_blockhash,
-    );
-
-    // Process the transaction.
-    let err = context
-        .banks_client
-        .process_transaction(validate_tx)
-        .await
-        .expect_err("validation should fail");
+    // Fail to validate Transfer operation.
+    let err = process_failing_validate_ix(&mut context, validate_ix, vec![&second_signer]).await;
 
     // Deconstruct the error code and make sure it is what we expect.
     match err {
@@ -301,12 +234,6 @@ async fn test_pass() {
     // --------------------------------
     // Create RuleSet
     // --------------------------------
-    // Find RuleSet PDA.
-    let (rule_set_addr, _rule_set_bump) = mpl_token_auth_rules::pda::find_rule_set_address(
-        context.payer.pubkey(),
-        "test rule_set".to_string(),
-    );
-
     // Create a Pass Rule.
     let pass_rule = Rule::Pass;
 
@@ -318,36 +245,9 @@ async fn test_pass() {
 
     println!("{:#?}", rule_set);
 
-    // Serialize the RuleSet using RMP serde.
-    let mut serialized_rule_set = Vec::new();
-    rule_set
-        .serialize(&mut Serializer::new(&mut serialized_rule_set))
-        .unwrap();
-
-    // Create a `create` instruction.
-    let create_ix = CreateOrUpdateBuilder::new()
-        .payer(context.payer.pubkey())
-        .rule_set_pda(rule_set_addr)
-        .build(CreateOrUpdateArgs::V1 {
-            serialized_rule_set,
-        })
-        .unwrap()
-        .instruction();
-
-    // Add it to a transaction.
-    let create_tx = Transaction::new_signed_with_payer(
-        &[create_ix],
-        Some(&context.payer.pubkey()),
-        &[&context.payer],
-        context.last_blockhash,
-    );
-
-    // Process the transaction.
-    context
-        .banks_client
-        .process_transaction(create_tx)
-        .await
-        .expect("creation should succeed");
+    // Put the RuleSet on chain.
+    let rule_set_addr =
+        create_rule_set_on_chain(&mut context, rule_set, "test rule_set".to_string()).await;
 
     // --------------------------------
     // Validate Pass Rule
@@ -371,34 +271,13 @@ async fn test_pass() {
         .unwrap()
         .instruction();
 
-    // Add it to a transaction.
-    let validate_tx = Transaction::new_signed_with_payer(
-        &[validate_ix],
-        Some(&context.payer.pubkey()),
-        &[&context.payer],
-        context.last_blockhash,
-    );
-
-    // Process the transaction.
-    context
-        .banks_client
-        .process_transaction(validate_tx)
-        .await
-        .expect("validation should succeed");
+    // Validate Transfer operation.
+    process_passing_validate_ix(&mut context, validate_ix, vec![]).await;
 }
 
 #[tokio::test]
 async fn test_update_ruleset() {
     let mut context = program_test().start_with_context().await;
-
-    // --------------------------------
-    // Create RuleSet
-    // --------------------------------
-    // Find RuleSet PDA.
-    let (rule_set_addr, _rule_set_bump) = mpl_token_auth_rules::pda::find_rule_set_address(
-        context.payer.pubkey(),
-        "test rule_set".to_string(),
-    );
 
     // Create a Pass Rule.
     let pass_rule = Rule::Pass;
@@ -409,38 +288,11 @@ async fn test_update_ruleset() {
         .add(Operation::Transfer.to_string(), pass_rule)
         .unwrap();
 
-    // Serialize the RuleSet using RMP serde.
-    let mut serialized_rule_set = Vec::new();
-    rule_set
-        .serialize(&mut Serializer::new(&mut serialized_rule_set))
-        .unwrap();
+    // Put the RuleSet on chain.
+    let _rule_set_addr =
+        create_rule_set_on_chain(&mut context, rule_set, "test rule_set".to_string()).await;
 
-    // Create a `create` instruction.
-    let create_ix = CreateOrUpdateBuilder::new()
-        .payer(context.payer.pubkey())
-        .rule_set_pda(rule_set_addr)
-        .build(CreateOrUpdateArgs::V1 {
-            serialized_rule_set,
-        })
-        .unwrap()
-        .instruction();
-
-    // Add it to a transaction.
-    let create_tx = Transaction::new_signed_with_payer(
-        &[create_ix.clone()],
-        Some(&context.payer.pubkey()),
-        &[&context.payer],
-        context.last_blockhash,
-    );
-
-    // Process the transaction.
-    context
-        .banks_client
-        .process_transaction(create_tx)
-        .await
-        .expect("creation should succeed");
-
-    // Create some additional rules.
+    // Create some other rules.
     let adtl_signer = Rule::AdditionalSigner {
         account: context.payer.pubkey(),
     };
@@ -460,34 +312,7 @@ async fn test_update_ruleset() {
         .add(Operation::Transfer.to_string(), overall_rule)
         .unwrap();
 
-    // Serialize the RuleSet using RMP serde.
-    let mut serialized_rule_set = Vec::new();
-    rule_set
-        .serialize(&mut Serializer::new(&mut serialized_rule_set))
-        .unwrap();
-
-    // Create a new `create` instruction to update the RuleSet.
-    let update_ix = CreateOrUpdateBuilder::new()
-        .payer(context.payer.pubkey())
-        .rule_set_pda(rule_set_addr)
-        .build(CreateOrUpdateArgs::V1 {
-            serialized_rule_set,
-        })
-        .unwrap()
-        .instruction();
-
-    // Add it to a transaction.
-    let update_tx = Transaction::new_signed_with_payer(
-        &[update_ix],
-        Some(&context.payer.pubkey()),
-        &[&context.payer],
-        context.last_blockhash,
-    );
-
-    // Process the transaction.
-    context
-        .banks_client
-        .process_transaction(update_tx)
-        .await
-        .expect("Updating RuleSet PDA with new RuleSet should succeed");
+    // Put the updated RuleSet on chain.
+    let _rule_set_addr =
+        create_rule_set_on_chain(&mut context, rule_set, "test rule_set".to_string()).await;
 }
--- program/tests/utils/mod.rs
+++ program/tests/utils/mod.rs
@@ -1,5 +1,13 @@
+use mpl_token_auth_rules::{
+    instruction::{builders::CreateOrUpdateBuilder, CreateOrUpdateArgs, InstructionBuilder},
+    state::RuleSet,
+};
 use num_derive::ToPrimitive;
-use solana_program_test::ProgramTest;
+use rmp_serde::Serializer;
+use serde::Serialize;
+use solana_program::{instruction::Instruction, pubkey::Pubkey};
+use solana_program_test::{BanksClientError, ProgramTest, ProgramTestContext};
+use solana_sdk::{signature::Signer, signer::keypair::Keypair, transaction::Transaction};
 
 #[repr(C)]
 #[derive(ToPrimitive)]
@@ -21,4 +29,95 @@ impl ToString for Operation {
 
 pub fn program_test() -> ProgramTest {
     ProgramTest::new("mpl_token_auth_rules", mpl_token_auth_rules::id(), None)
+}
+
+pub async fn create_rule_set_on_chain(
+    context: &mut ProgramTestContext,
+    rule_set: RuleSet,
+    rule_set_name: String,
+) -> Pubkey {
+    // Find RuleSet PDA.
+    let (rule_set_addr, _rule_set_bump) =
+        mpl_token_auth_rules::pda::find_rule_set_address(context.payer.pubkey(), rule_set_name);
+
+    // Serialize the RuleSet using RMP serde.
+    let mut serialized_rule_set = Vec::new();
+    rule_set
+        .serialize(&mut Serializer::new(&mut serialized_rule_set))
+        .unwrap();
+
+    // Create a `create` instruction.
+    let create_ix = CreateOrUpdateBuilder::new()
+        .payer(context.payer.pubkey())
+        .rule_set_pda(rule_set_addr)
+        .build(CreateOrUpdateArgs::V1 {
+            serialized_rule_set,
+        })
+        .unwrap()
+        .instruction();
+
+    // Add it to a transaction.
+    let create_tx = Transaction::new_signed_with_payer(
+        &[create_ix],
+        Some(&context.payer.pubkey()),
+        &[&context.payer],
+        context.last_blockhash,
+    );
+
+    // Process the transaction.
+    context
+        .banks_client
+        .process_transaction(create_tx)
+        .await
+        .expect("creation should succeed");
+
+    rule_set_addr
+}
+
+pub async fn process_passing_validate_ix(
+    context: &mut ProgramTestContext,
+    validate_ix: Instruction,
+    additional_signers: Vec<&Keypair>,
+) {
+    let mut signing_keypairs = vec![&context.payer];
+    signing_keypairs.extend(additional_signers);
+
+    // Add ix to a transaction.
+    let validate_tx = Transaction::new_signed_with_payer(
+        &[validate_ix],
+        Some(&context.payer.pubkey()),
+        &signing_keypairs,
+        context.last_blockhash,
+    );
+
+    // Process the transaction.
+    context
+        .banks_client
+        .process_transaction(validate_tx)
+        .await
+        .expect("Validation should succeed");
+}
+
+pub async fn process_failing_validate_ix(
+    context: &mut ProgramTestContext,
+    validate_ix: Instruction,
+    additional_signers: Vec<&Keypair>,
+) -> BanksClientError {
+    let mut signing_keypairs = vec![&context.payer];
+    signing_keypairs.extend(additional_signers);
+
+    // Add ix to a transaction.
+    let validate_tx = Transaction::new_signed_with_payer(
+        &[validate_ix],
+        Some(&context.payer.pubkey()),
+        &signing_keypairs,
+        context.last_blockhash,
+    );
+
+    // Process the transaction.
+    context
+        .banks_client
+        .process_transaction(validate_tx)
+        .await
+        .expect_err("validation should fail")
 }
