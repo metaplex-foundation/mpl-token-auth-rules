f7211403fb38310f83c14de1df4d6bd39d6dada2
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2022-12-23 17:25:32 -0800

    Add fail and pass test for All rule

---
+++ program/tests/all.rs
@@ -0,0 +1,108 @@
+#![cfg(feature = "test-bpf")]
+
+pub mod utils;
+
+use mpl_token_auth_rules::{
+    error::RuleSetError,
+    instruction::{builders::ValidateBuilder, InstructionBuilder, ValidateArgs},
+    payload::{Payload, PayloadKey, PayloadType},
+    state::{CompareOp, Rule, RuleSet},
+};
+use solana_program::instruction::AccountMeta;
+use solana_program_test::tokio;
+use solana_sdk::{signature::Signer, signer::keypair::Keypair};
+use utils::{
+    assert_rule_set_error, create_rule_set_on_chain, process_failing_validate_ix,
+    process_passing_validate_ix, program_test, Operation,
+};
+
+#[tokio::test]
+async fn test_all() {
+    let mut context = program_test().start_with_context().await;
+
+    // --------------------------------
+    // Create RuleSet
+    // --------------------------------
+    // Create some rules.
+    let second_signer = Keypair::new();
+    let adtl_signer = Rule::AdditionalSigner {
+        account: second_signer.pubkey(),
+    };
+
+    let amount_check = Rule::Amount {
+        amount: 5,
+        operator: CompareOp::Lt,
+    };
+
+    let overall_rule = Rule::All {
+        rules: vec![adtl_signer, amount_check],
+    };
+
+    // Create a RuleSet.
+    let mut rule_set = RuleSet::new("test rule_set".to_string(), context.payer.pubkey());
+    rule_set
+        .add(Operation::Transfer.to_string(), overall_rule)
+        .unwrap();
+
+    println!("{:#?}", rule_set);
+
+    // Put the RuleSet on chain.
+    let rule_set_addr =
+        create_rule_set_on_chain(&mut context, rule_set, "test rule_set".to_string()).await;
+
+    // --------------------------------
+    // Validate fail
+    // --------------------------------
+    // Create a Keypair to simulate a token mint address.
+    let mint = Keypair::new().pubkey();
+
+    // Store a payload of data with the WRONG amount.
+    let payload = Payload::from([(PayloadKey::Amount, PayloadType::Number(5))]);
+
+    // Create a `validate` instruction with the additional signer but sending WRONG amount.
+    let validate_ix = ValidateBuilder::new()
+        .rule_set_pda(rule_set_addr)
+        .mint(mint)
+        .additional_rule_accounts(vec![AccountMeta::new_readonly(
+            second_signer.pubkey(),
+            true,
+        )])
+        .build(ValidateArgs::V1 {
+            operation: Operation::Transfer.to_string(),
+            payload: payload.clone(),
+            update_rule_state: false,
+        })
+        .unwrap()
+        .instruction();
+
+    // Fail to validate Transfer operation.
+    let err = process_failing_validate_ix(&mut context, validate_ix, vec![&second_signer]).await;
+
+    // Check that error is what we expect.  In this case we expect the first failure to roll up.
+    assert_rule_set_error(err, RuleSetError::AmountCheckFailed);
+
+    // --------------------------------
+    // Validate pass
+    // --------------------------------
+    // Store a payload of data with the CORRECT amount.
+    let payload = Payload::from([(PayloadKey::Amount, PayloadType::Number(4))]);
+
+    // Create a `validate` instruction with the additional signer AND sending CORRECT amount.
+    let validate_ix = ValidateBuilder::new()
+        .rule_set_pda(rule_set_addr)
+        .mint(mint)
+        .additional_rule_accounts(vec![AccountMeta::new_readonly(
+            second_signer.pubkey(),
+            true,
+        )])
+        .build(ValidateArgs::V1 {
+            operation: Operation::Transfer.to_string(),
+            payload,
+            update_rule_state: false,
+        })
+        .unwrap()
+        .instruction();
+
+    // Validate Transfer operation since both Rule conditions were true.
+    process_passing_validate_ix(&mut context, validate_ix, vec![&second_signer]).await;
+}
