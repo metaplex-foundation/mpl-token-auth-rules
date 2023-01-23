ca583258b775cf0a7d9d9febf2171b11bfc83ac6
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2023-01-23 13:01:46 -0800

    Adding feature gating for serde_with

--- program/src/state/rule_set.rs
+++ program/src/state/rule_set.rs
@@ -2,6 +2,7 @@
 use crate::{error::RuleSetError, state::Rule};
 use borsh::{BorshDeserialize, BorshSerialize};
 use serde::{Deserialize, Serialize};
+#[cfg(feature = "serde-feature")]
 use serde_with::{As, DisplayFromStr};
 use solana_program::{entrypoint::ProgramResult, pubkey::Pubkey};
 use std::collections::HashMap;
