efc011a024a0b1ead715964905ae4beec6f56dd5
blockiosaurus <blockiosaurus@gmail.com>
2023-01-24 09:48:33 -0500

    Adding the PDA function to the SDK.

--- packages/sdk/src/mpl-token-authorization-rules.ts
+++
@@ -1,5 +0,0 @@
-export * from './errors';
-// @ts-ignore
-export * from './generated';
-
-export const PREFIX = 'rule_set';
---
+++ packages/sdk/src/pda.ts
@@ -0,0 +1,9 @@
+import { PublicKey } from '@solana/web3.js';
+import { PREFIX, PROGRAM_ID } from './mpl-token-auth-rules';
+
+export const findRuleSetPDA = async (payer: PublicKey, name: string) => {
+  return await PublicKey.findProgramAddress(
+    [Buffer.from(PREFIX), payer.toBuffer(), Buffer.from(name)],
+    PROGRAM_ID,
+  );
+};
