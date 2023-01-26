5fe7f53b196a34b727d3ba820d6d0995e8560315
Michael Danenberg <56533526+danenbm@users.noreply.github.com>
2023-01-26 13:17:36 -0800

    Add test for wrong PDA sent to create instruction

--- program/tests/integration.rs
+++ program/tests/integration.rs
@@ -202,3 +202,62 @@ async fn test_composed_rule() {
     // Check that error is what we expect.
     assert_custom_error!(err, RuleSetError::AmountCheckFailed);
 }
+
+#[tokio::test]
+async fn test_rule_set_creation_wrong_pda_fails() {
+    let mut context = program_test().start_with_context().await;
+
+    // --------------------------------
+    // Create RuleSet
+    // --------------------------------
+    // Create some rules.
+    let adtl_signer = Rule::AdditionalSigner {
+        account: context.payer.pubkey(),
+    };
+
+    // Create a RuleSet.
+    let mut rule_set = RuleSetV1::new("test rule_set".to_string(), context.payer.pubkey());
+    rule_set
+        .add(Operation::OwnerTransfer.to_string(), adtl_signer)
+        .unwrap();
+
+    // Find RuleSet PDA using WRONG name for seed.
+    let (wrong_rule_set_pda, _rule_set_bump) = mpl_token_auth_rules::pda::find_rule_set_address(
+        context.payer.pubkey(),
+        "WRONG NAME".to_string(),
+    );
+
+    // Serialize the RuleSet using RMP serde.
+    let mut serialized_rule_set = Vec::new();
+    rule_set
+        .serialize(&mut Serializer::new(&mut serialized_rule_set))
+        .unwrap();
+
+    // Create a `create` instruction.
+    let create_ix = CreateOrUpdateBuilder::new()
+        .payer(context.payer.pubkey())
+        .rule_set_pda(wrong_rule_set_pda)
+        .build(CreateOrUpdateArgs::V1 {
+            serialized_rule_set,
+        })
+        .unwrap()
+        .instruction();
+
+    // Add it to a transaction.
+    let create_tx = Transaction::new_signed_with_payer(
+        &[create_ix],
+        Some(&context.payer.pubkey()),
+        &[&context.payer],
+        context.last_blockhash,
+    );
+
+    // Process the transaction.
+    let err = context
+        .banks_client
+        .process_transaction(create_tx)
+        .await
+        .expect_err("Creation should fail");
+
+    // Check that error is what we expect.
+    assert_custom_error!(err, RuleSetError::DerivedKeyInvalid);
+}
