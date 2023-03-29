8f99d0343fca129eca6c335f2a341c4683cff7d8
febo <febo@metaplex.com>
2023-03-29 12:11:53 +0100

    Add update a ruleset from v2 to v1 test

--- packages/sdk/test/createRulesetV2.test.ts
+++ packages/sdk/test/createRulesetV2.test.ts
@@ -84,3 +84,56 @@ test('it can update a ruleset from v1 to v2', async (t) => {
   ) as RuleSetV2;
   t.deepEqual(updatedLatestDeserializedRuleSet, ruleSetV2);
 });
+
+test('it can update a ruleset from v2 to v1', async (t) => {
+  // Given a serialized ruleset v2 account data.
+  const { connection, payer } = await getConnectionAndPayer();
+  const name = 'My Rule Set';
+
+  const publicKeyA = Keypair.generate().publicKey;
+  const publicKeyB = Keypair.generate().publicKey;
+  const ruleSetV2: RuleSetV2 = {
+    name,
+    owner: payer.publicKey,
+    operations: {
+      deposit: additionalSignerV2(publicKeyA),
+      withdraw: additionalSignerV2(publicKeyB),
+    },
+  };
+  const serializedRuleSetV2 = serializeRuleSetV2(ruleSetV2);
+
+  // When we create a new ruleset account using the v2 data.
+  const ruleSetPda = await createOrUpdateRuleset(connection, payer, name, serializedRuleSetV2);
+
+  // Then the latest ruleset is a ruleset v2.
+  const updatedRawRuleSetPdaAccount = await connection.getAccountInfo(ruleSetPda);
+  const updatedLatestDeserializedRuleSet = getLatestRuleSet(
+    updatedRawRuleSetPdaAccount?.data,
+  ) as RuleSetV2;
+  t.deepEqual(updatedLatestDeserializedRuleSet, ruleSetV2);
+
+  // Additionally, Given a ruleset v1 account data.
+
+  const ruleSetV1 = {
+    libVersion: 1,
+    ruleSetName: name,
+    owner: Array.from(payer.publicKey.toBytes()),
+    operations: {
+      Transfer: {
+        ProgramOwned: {
+          program: Array.from(payer.publicKey.toBytes()),
+          field: 'Destination',
+        },
+      },
+    },
+  };
+  const serializedRuleSetV1 = encode(ruleSetV1);
+
+  // When we update the ruleset account using the v1 data.
+  await createOrUpdateRuleset(connection, payer, name, serializedRuleSetV1);
+
+  // Then the latest ruleset is a ruleset v1.
+  const rawRuleSetPdaAccount = await connection.getAccountInfo(ruleSetPda);
+  const latestDeserializedRuleSet = getLatestRuleSet(rawRuleSetPdaAccount?.data) as string;
+  t.is(latestDeserializedRuleSet, JSON.stringify(ruleSetV1, null, 2));
+});
