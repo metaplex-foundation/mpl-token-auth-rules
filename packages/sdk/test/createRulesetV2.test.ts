413aa162d108db4ae1119cd6a7fa8f900d6166ad
febo <febo@metaplex.com>
2023-03-30 14:33:22 +0100

    Add writeToBuffer and puffRuleSet instructions

--- packages/sdk/test/createRulesetV2.test.ts
+++ packages/sdk/test/createRulesetV2.test.ts
@@ -4,10 +4,16 @@ import test from 'ava';
 import {
   additionalSignerV2,
   getLatestRuleSet,
+  pubkeyListMatchV2,
   RuleSetV2,
   serializeRuleSetV2,
 } from '../src/mpl-token-auth-rules';
-import { createOrUpdateRuleset, getConnectionAndPayer } from './_setup';
+import {
+  createOrUpdateLargeRuleset,
+  createOrUpdateRuleset,
+  getConnectionAndPayer,
+  writeAndPuff,
+} from './_setup';
 
 test('it can create a ruleset v2', async (t) => {
   // Given a serialized ruleset v2 account data.
@@ -136,4 +142,57 @@ test('it can update a ruleset from v2 to v1', async (t) => {
   const rawRuleSetPdaAccount = await connection.getAccountInfo(ruleSetPda);
   const latestDeserializedRuleSet = getLatestRuleSet(rawRuleSetPdaAccount?.data) as string;
   t.is(latestDeserializedRuleSet, JSON.stringify(ruleSetV1, null, 2));
+});
+
+test('it can create a ruleset v2 from a buffer account', async (t) => {
+  // Given a serialized ruleset v2 account data.
+  const { connection, payer } = await getConnectionAndPayer();
+  const publicKeyA = Keypair.generate().publicKey;
+  const publicKeyB = Keypair.generate().publicKey;
+  const name = 'My Rule Set';
+  const ruleSet: RuleSetV2 = {
+    name,
+    owner: payer.publicKey,
+    operations: {
+      deposit: additionalSignerV2(publicKeyA),
+      withdraw: additionalSignerV2(publicKeyB),
+    },
+  };
+  const serializedRuleSet = serializeRuleSetV2(ruleSet);
+
+  // Creating a buffer account.
+  const bufferPda = await writeAndPuff(connection, payer, name, serializedRuleSet);
+
+  // When we create a new ruleset account using it.
+  const ruleSetPda = await createOrUpdateRuleset(connection, payer, name, bufferPda);
+
+  // Then we can deserialize the account data and get the same ruleset.
+  const rawRuleSetPdaAccount = await connection.getAccountInfo(ruleSetPda);
+  const deserializedRuleSet = getLatestRuleSet(rawRuleSetPdaAccount?.data) as RuleSetV2;
+  t.deepEqual(deserializedRuleSet, ruleSet);
+});
+
+test('it can create a large ruleset v2 from a buffer account', async (t) => {
+  // Given a serialized ruleset v2 account data.
+  const { connection, payer } = await getConnectionAndPayer();
+  const name = 'My Large Rule Set';
+  const ruleSet: RuleSetV2 = {
+    name,
+    owner: payer.publicKey,
+    operations: {
+      transfer: pubkeyListMatchV2(
+        'Destination',
+        [...Array(350)].map(() => Keypair.generate().publicKey),
+      ),
+    },
+  };
+  const serializedRuleSet = serializeRuleSetV2(ruleSet);
+
+  // When we create a new ruleset account using it.
+  const ruleSetPda = await createOrUpdateLargeRuleset(connection, payer, name, serializedRuleSet);
+
+  // Then we can deserialize the account data and get the same ruleset.
+  const rawRuleSetPdaAccount = await connection.getAccountInfo(ruleSetPda);
+  const deserializedRuleSet = getLatestRuleSet(rawRuleSetPdaAccount?.data) as RuleSetV2;
+  t.deepEqual(deserializedRuleSet, ruleSet);
 });
