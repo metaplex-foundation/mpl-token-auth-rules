ef465b77333c157e152f2b11a00ab750ced948c9
ngundotra <noah@gundotra.org>
2023-04-23 18:06:25 -0400

    add RuleSetV1 typing

--- packages/sdk/src/mpl-token-auth-rules.ts
+++ packages/sdk/src/mpl-token-auth-rules.ts
@@ -4,6 +4,9 @@ import {
   ruleSetHeaderBeet,
   ruleSetRevisionMapV1Beet,
 } from './generated';
+import {
+  PublicKey
+} from '@solana/web3.js'
 
 import { decode } from '@msgpack/msgpack';
 
@@ -31,9 +34,41 @@ export const getRevisionMapV1 = (data: Buffer): RuleSetRevisionMapV1 => {
 export const getLatestRuleSet = (data: Buffer): any => {
   const header = getHeader(data);
   const revmap = getRevisionMapV1(data);
-  const latestRevision = parseInt(revmap.ruleSetRevisions[revmap.ruleSetRevisions.length - 1]);
+  const latestRevision = parseInt(
+    revmap.ruleSetRevisions[revmap.ruleSetRevisions.length - 1] as any
+  );
   const rulesetDecoded = decode(
-    data.slice(latestRevision + 1, parseInt(header.revMapVersionLocation)),
+    data.slice(
+      latestRevision + 1,
+      parseInt(header.revMapVersionLocation as any)
+    )
   );
   return JSON.stringify(rulesetDecoded, null, 2);
+}
+
+export type RuleSetV1 = {
+  version: number;
+  owner: PublicKey;
+  name: string;
+  ruleset: Record<string, string>;
+};
+
+export const getLatestRuleSetV1 = (data: Buffer): RuleSetV1 => {
+  const header = getHeader(data);
+  const revmap = getRevisionMapV1(data);
+  const latestRevision = parseInt(
+    revmap.ruleSetRevisions[revmap.ruleSetRevisions.length - 1] as any
+  );
+  const rulesetDecoded = decode(
+    data.slice(
+      latestRevision + 1,
+      parseInt(header.revMapVersionLocation as any)
+    )
+  );
+  return {
+    version: rulesetDecoded[0] as number,
+    owner: new PublicKey(rulesetDecoded[1]),
+    name: rulesetDecoded[2] as string,
+    ruleset: rulesetDecoded[3] as Record<string, string>,
+  };
 };
