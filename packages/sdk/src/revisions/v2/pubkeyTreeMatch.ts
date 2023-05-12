c236a344cf7fa3659baed8ab2c36fcb7bc819c70
febo <febo@metaplex.com>
2023-05-12 16:49:52 +0100

    Use raw array type

--- packages/sdk/src/revisions/v2/pubkeyTreeMatch.ts
+++ packages/sdk/src/revisions/v2/pubkeyTreeMatch.ts
@@ -6,21 +6,21 @@ export type PubkeyTreeMatchRuleV2 = {
   type: 'PubkeyTreeMatch';
   pubkeyField: string;
   proofField: string;
-  root: Uint8Array;
+  root: number[];
 };
 
 export const pubkeyTreeMatchV2 = (
   pubkeyField: string,
   proofField: string,
-  root: Uint8Array,
-): PubkeyTreeMatchRuleV2 => ({ type: 'PubkeyTreeMatch', pubkeyField, proofField, root });
+  root: Uint8Array | number[],
+): PubkeyTreeMatchRuleV2 => ({ type: 'PubkeyTreeMatch', pubkeyField, proofField, root: [...root] });
 
 export const serializePubkeyTreeMatchV2 = (rule: PubkeyTreeMatchRuleV2): Buffer => {
   return Buffer.concat([
     serializeRuleHeaderV2(RuleTypeV2.PubkeyTreeMatch, 96),
     serializeString32(rule.pubkeyField),
     serializeString32(rule.proofField),
-    rule.root,
+    new Uint8Array(rule.root),
   ]);
 };
 
@@ -30,7 +30,7 @@ export const deserializePubkeyTreeMatchV2 = (buffer: Buffer, offset = 0): Pubkey
   offset += 32;
   const proofField = deserializeString32(buffer, offset);
   offset += 32;
-  const root = new Uint8Array(buffer.subarray(offset, offset + 32));
+  const root = new Uint8Array(buffer.subarray(offset, offset + 32)) ;
 
   return pubkeyTreeMatchV2(pubkeyField, proofField, root);
 };
